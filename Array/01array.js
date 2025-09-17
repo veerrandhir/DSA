6623e75327fb1b3ab4b9139e639f8d6a890d665a
Randhir Kumar <veerrandhir3@gmail.com>
2025-09-17 13:01:26 +0530

    Two pointer swap Questions

--- Array/01array.js
+++ Array/01array.js
@@ -107,3 +107,24 @@ for (let i = 0; i < 5; i++) {
 }
 console.log(arr);
 */
+
+// Swap and arrange all element in group
+/*
+let arr = [1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1];
+
+let i = 0,
+  j = 0;
+
+while (i <= arr.length) {
+  if (arr[i] === 0) {
+    let temp;
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j++;
+  }
+  i++;
+}
+
+console.log(arr);
+*/
